b3c381f324db492d55760851b8b8544cb8447399
TaiTan291 <dachnegfujing41@gmail.com>
2026-05-24 17:22:10 +0900

    fix css html

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.html", "./src/**/*.rs"],
-  theme: {
-    extend: {},
+	theme: {
+    extend: {
+      colors: {
+        cyber: {
+          // 背景・ベース
+          page:      "##e2e8f0",  // ページ背景
+          container: "#020617",  // コンテナ / Section
+          surface:   "#0f172a",  // Header / Footer
+					block:     "#242426",  // block
+					layout:    "#0d2137",
+
+          // アクセント
+          accent:    "#FFFFFF",  // メインアクセント（見出し・ボーダー・タグ）
+          link:      "#4499ff",  // サブアクセント（リンク・ホバー）
+
+          // テキスト
+          text:      "#FFFFFF",  // メインテキスト
+          muted:     "#FFFFFF",  // サブテキスト
+        },
+      },
+    },
   },
   plugins: [],
 }
